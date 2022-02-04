d879d6cc6b668b4731ebcdec37a136fe7bd4ee84
Alperen Yeşil <alperensyesil@gmail.com>
2022-02-04 03:37:09 +0300

    Katmanalrda Güncellemeler Yapıldı

---
+++ DataAccessLayer/Concrete/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            string connectionString = "Data Source=(LocalDb)\\MSSQLLocalDB; Initial Catalog=CoreBlogDb; Integrated Security=SSPI;";
+
+            builder.UseSqlServer(connectionString);
+        }
+
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        AppDbContext db = new AppDbContext();
+        public void Delete(T item)
+        {
+            db.Remove(item);
+            db.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            return db.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            return db.Set<T>().ToList();
+        }
+
+        public void Insert(T item)
+        {
+            db.Add(item);
+            db.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            db.Update(item);
+            db.SaveChanges();
+        }
+    }
+}
