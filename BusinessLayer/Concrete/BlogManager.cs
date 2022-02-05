31eff2907447b2f8d6bbfe245426215fa2f03e07
Alperen Yeşil <alperensyesil@gmail.com>
2022-02-05 20:31:41 +0300

    Son task yansiltildi

---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDal _blogDal;
+
+        public BlogManager(IBlogDal blogDal)
+        {
+            this._blogDal = blogDal;
+        }
+
+        public void BlogAdd(Blog blog)
+        {
+            _blogDal.Insert(blog);
+        }
+
+        public void BlogRemove(Blog blog)
+        {
+            _blogDal.Delete(blog);
+        }
+
+        public void BlogUpdate(Blog blog)
+        {
+            _blogDal.Update(blog);
+        }
+
+        public List<Blog> GetBlogListWithCategory()
+        {
+            return _blogDal.GetListWithCategory();
+        }
+
+        public Blog GetById(int id)
+        {
+            return _blogDal.GetById(id); 
+        }
+
+        public List<Blog> GetBlogByID(int id)
+        {
+            return _blogDal.GetListAll(x => x.BlogID == id);
+        }
+
+        public List<Blog> GetList()
+        {
+            return _blogDal.GetListAll();
+        }
+    }
+}
--- DataAccessLayer/Repositories/GenericRepository.cs
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +21,11 @@ namespace DataAccessLayer.Repositories
         public T GetById(int id)
         {
             return db.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll(Expression<Func<T, bool>> filter)
+        {
+            return db.Set<T>().Where(filter).ToList();
         }
 
         public List<T> GetListAll()
